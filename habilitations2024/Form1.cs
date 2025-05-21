4097606bb5000817b39b459108ad8c55e3d2faa0
Asukiri1 <ducfilippo@gmail.com>
2025-05-21 17:50:20 +0200

    connection a la bdd waampserver

--- habilitations2024/Form1.cs
+++ habilitations2024/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace habilitations2024
 {
@@ -15,6 +16,30 @@ namespace habilitations2024
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost;Database=habilitations;User Id=habilitations;Password=motdepasseuser;";
+            MySqlConnection connection = null;
+
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur de connexion : \n" + ex.Message, "Erreur MySQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
         }
     }
 }
